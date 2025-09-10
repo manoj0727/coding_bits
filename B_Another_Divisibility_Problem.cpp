28b738351cb81245bd32d854142f8e22ce96bacf
manoj0727 <manojkumawat2465@gmail.com>
2025-09-10 05:57:20 +0530

    update cpp

---
+++ B_Another_Divisibility_Problem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int mod = 1e9+7;
+ 
+void solve() {
+    long long int x;
+    cin>>x;
+    cout<<2*x<<endl;
+ 
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ cp/index.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int mod = 1e9+7;
+ 
+void solve() {
+    cout<<"HELLO"<<endl;
+ 
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
